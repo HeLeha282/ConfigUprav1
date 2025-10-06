28c5e0fca43f9602abdea4a124a39a6b862d82a2
HeLeha282 <aleksejnadmitov696@gmail.com>
2025-10-06 22:37:37 +0300

    Этап 2. Конфигурация. Complited!

--- ConfigUprav1/ConfigUprav1.cpp
+++ ConfigUprav1/ConfigUprav1.cpp
@@ -4,60 +4,138 @@
 #include <vector>
 #include <sstream>
 #include <Windows.h>
+#include <fstream>
+
+//2-config: promt - приглашение к вводу. vfs - путь к физ расположению vfs
+std::string vfsPath;
+std::string promt;
+std::string startScriptPath;
+std::string vfsName = "vfs";
+std::string curDir = "~";
+
+
+
+
+void getAndProcessInputLineFromCmd();
+void getAndProcessInputLineFromScript(std::ifstream& inf);
+void processScriptFile(std::string scriptPath);
+
 void cd(std::vector<std::string> vecArgs);
 void ls(std::vector<std::string> vecArgs);
 void exit(std::vector<std::string> vecArgs);
 
-std::vector<std::string> vectorArg(std::string);
-int main()
+
+//возвращаемый_тип (*имя_указателя)(параметры)
+//typedef void(*FunctionPtr)();
+using FunctionPtr = void(*)(std::vector<std::string>);
+
+std::unordered_map<std::string, FunctionPtr> mapCommand = {
+    {"cd", &cd},
+    {"ls", &ls},
+    {"exit", &exit}
+};
+
+std::vector<std::string> vectorArg(std::string input);
+int main(int argc, char* argv[])
 {
+
+
     SetConsoleCP(65001); // UTF-8
     SetConsoleOutputCP(65001); // UTF-8
     setlocale(LC_ALL, "ru-RU");
 
-    //возвращаемый_тип (*имя_указателя)(параметры)
-    //typedef void(*FunctionPtr)();
-    using FunctionPtr = void(*)(std::vector<std::string>);
 
-    std::unordered_map<std::string, FunctionPtr> mapCommand = {
-        {"cd", &cd},
-        {"ls", &ls},
-        {"exit", &exit}
-    };
+    std::string arg;
+
+    for (int i = 1; i < argc; i++) {
+        arg = argv[i];
+        //std::cout << arg << std::endl;
+        if (arg == "--vfsPath") {
+            i++;
+            vfsPath = argv[i];
+        }
+        else if (arg == "--promt") {
+            i++;
+            promt = argv[i];
+        }
+        else if (arg == "--startScriptPath") {
+            i++;
+            startScriptPath = argv[i];
+        }
+    }
+    std::cout << "vfsPath = " << vfsPath << '\n'
+        << "promt = " << promt << '\n'
+        << "startScriptPath = " << startScriptPath << "\n\n";
+    
+    if (!startScriptPath.empty())
+        processScriptFile(startScriptPath);
+
+
 
     //mapCommand["cd"]();
     //mapCommand["ls"]();
 
 
-    std::string vfsName = "vfs";
-    std::string curDir = "~";
+    
     while(true){
-        std::cout << vfsName << ':' << curDir << "$ ";
-        std::string input;
-        getline(std::cin, input);
-        //std::cout << "Вы ввели: " << input <<std::endl;
-
-        std::vector<std::string> vecArg = vectorArg(input);
-        /*std::cout << "ВНИМАНИЕ ВЕКТОР";
-        for (std::string element : vecArg) {
-            std::cout << "[" << element << "] ";
-        }*/
-        if (vecArg.size() != 0) {
-            
-            if (mapCommand.find(vecArg[0]) != mapCommand.end()) {
-                mapCommand[vecArg[0]](vecArg);
-            }
-            else {
-                std::cout << "Unsupported command: " << vecArg[0]<<std::endl;
-            }
-        }
-        //else {
-        //}
-        //std::cout<<std::endl;
+        getAndProcessInputLineFromCmd();
     }
     return 0;
 }
 
+void getAndProcessInputLineFromCmd() {
+    std::cout << promt << ':' << curDir << "$ ";
+    std::string input;
+    getline(std::cin, input);
+
+    std::vector<std::string> vecArg = vectorArg(input);
+    /*std::cout << "ВНИМАНИЕ ВЕКТОР";
+    for (std::string element : vecArg) {
+        std::cout << "[" << element << "] ";
+    }*/
+    if (vecArg.size() != 0) {
+
+        if (mapCommand.find(vecArg[0]) != mapCommand.end()) {
+            mapCommand[vecArg[0]](vecArg);
+        }
+        else {
+            std::cout << "Unsupported command: " << vecArg[0] << std::endl;
+        }
+    }
+}
+
+void getAndProcessInputLineFromScript(std::ifstream& inf) {
+    std::string input;
+    getline(inf, input);
+    if (input.substr(0, 2) == "//") {
+        return;
+    }
+
+    std::cout << promt << ':' << curDir << "$ " << input << std::endl;
+    std::vector<std::string> vecArg = vectorArg(input);
+    if (vecArg.size() != 0) {
+
+        if (mapCommand.find(vecArg[0]) != mapCommand.end()) {
+            mapCommand[vecArg[0]](vecArg);
+        }
+        else {
+            std::cout << "Unsupported command: " << vecArg[0] << std::endl;
+        }
+    }
+}
+
+void processScriptFile(std::string scriptPath) {
+    std::ifstream inf(scriptPath);
+    if (!inf) {
+        std::cerr << "Ошибка при открытии файла скрипта" <<std::endl;
+    }
+    while (inf) {
+        getAndProcessInputLineFromScript(inf);
+    }
+    if (inf.bad()) {
+        std::cerr << "Возникла ошибка при работе с файлом скрипта"<<std::endl;
+    }
+};
 
 void cd(std::vector<std::string> vecArgs) {
     int vecArgsSize = vecArgs.size();
@@ -94,7 +172,7 @@ std::vector<std::string> vectorArg(std::string input) {
     std::istringstream iss(input);
     std::vector<std::string> vecArg;
     std::string arg;
-    std::string tmp;
+    std::string tmp;    
     bool flag = false;
     char ch;
     while (!iss.eof()) {
---
+++ OcScripts/script2.bat
@@ -0,0 +1,2 @@
+cd ../x64/Debug
+ConfigUprav1.exe --vfsPath C:\Users\Lesha --promt leha@hello --startScriptPath C:\Users\Lesha\Desktop\testScript.txt
